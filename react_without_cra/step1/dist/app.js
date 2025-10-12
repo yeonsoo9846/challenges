c554727cf4f0e27660dca36ec317307292c29b4c
김연수 <yeonsoo9846@gmail.com>
2025-10-12 11:04:26 +0900

    #feat : babel 설치/적용

---
+++ react_without_cra/step1/dist/app.js
@@ -0,0 +1,3 @@
+const App = () => /*#__PURE__*/React.createElement("h1", null, "React without cra");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(/*#__PURE__*/React.createElement(App, null));
---
+++ react_without_cra/step1/src/app.js
@@ -0,0 +1,3 @@
+const App = () => <h1>React without cra</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
